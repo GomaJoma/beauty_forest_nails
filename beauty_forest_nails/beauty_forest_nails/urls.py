83d7fc27c5adad60f32cbf5cc0cb06e79a7a1630
Joma <jomaGit@yandex.ru>
2021-10-07 19:08:57 +0300

    Reorganized static files structure. Modified about and services templates. Added about, index and services styles.

--- beauty_forest_nails/beauty_forest_nails/urls.py
+++ beauty_forest_nails/beauty_forest_nails/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('rules/', views.rules, name='rules'),
     path('reviews/', include('reviews.urls')),
-    path('about/', views.about_us, name='about_us'),
+    path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
     path('auth/', include('auth_reg.urls')),
     path('admin/', admin.site.urls),
--- beauty_forest_nails/beauty_forest_nails/views.py
+++ beauty_forest_nails/beauty_forest_nails/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html', None)
+    return render(request, 'index.html', None)
 
 
 def rules(request):
-    return render(request, 'main/rules.html', None)
+    return render(request, 'rules.html', None)
 
 
-def about_us(request):
-    return render(request, 'main/about_us.html', None)
+def about(request):
+    return render(request, 'about.html', None)
 
 
 def contacts(request):
-    return render(request, 'main/contacts.html', None)
+    return render(request, 'contacts.html', None)
